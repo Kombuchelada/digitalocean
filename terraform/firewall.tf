9e4cb5d125a327f122fa1238571174bfa6cc16bd
Daniel Lynch <106557636+Kombuchelada@users.noreply.github.com>
2025-12-28 05:54:04 -0800

    add nginx proxy manager

---
+++ terraform/firewall.tf
@@ -0,0 +1,44 @@
+resource "digitalocean_firewall" "www-1" {
+  name = "www-1-firewall"
+
+  droplet_ids = [digitalocean_droplet.www-1.id]
+
+  # Allow SSH
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow HTTP
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow HTTPS
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow all outbound
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
