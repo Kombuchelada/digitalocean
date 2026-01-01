51a7e7a4f5844ce833bb58f56d38c3798c130514
Daniel Lynch <106557636+Kombuchelada@users.noreply.github.com>
2026-01-01 12:15:13 -0800

    upgrade to 2GB

--- terraform/www-1.tf
+++ terraform/www-1.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "www-1" {
   image      = "ubuntu-24-04-x64"
   name       = "www-1"
   region     = "sfo2"
-  size       = "s-1vcpu-1gb"
+  size       = "s-1vcpu-2gb"
   ssh_keys   = [data.digitalocean_ssh_key.xXMacbookXx.id]
   monitoring = true
 
